0237689c470a1e8a626f9707ebc9bfa09e1c8513
Alex Yang <a.yang2@share.epsb.ca>
2018-04-14 15:23:56 +0000

    Added comments

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -4,25 +4,25 @@ public class PrimeNumbers
 {
 	public static void main(String args[])
 	{
-		int primeCount = 0;
-		double num;
+		int primeCount = 0; //counts the number of prime numbers that have been calculated 
+		double num; //number that is being tested
 		Boolean isPrime = true;
 		for (num = 1; primeCount < 1000 ;num += 2) //loops until 1000th prime number is reached
 		{
-			for (double i = 2; i <= num / 2; i++)
+			for (double i = 2; i <= num / 2; i++) //loops until program finds an even divisor
 			{
-				if (num % i == 0)
+				if (num % i == 0) //checks to see if remainder is 0
 				{
 				isPrime = false;
 				}
 			}
-			if (isPrime)
+			if (isPrime) //moves onto the next prime number
 				{
-				System.out.println(num);
+				//System.out.println(num); debugger, prints last found prime number
 				primeCount++;
 				}
 			else 
-				isPrime = true;
+				isPrime = true; //resets loop
 
 
 		}

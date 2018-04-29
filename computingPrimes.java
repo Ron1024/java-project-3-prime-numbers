0c02e58b109ebdbf107b28c01986fa6e7ac15f3b
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 14:50:59 -0600

    Created IF statements to check if number is prime

--- computingPrimes.java
+++ computingPrimes.java
@@ -6,10 +6,18 @@ public class computingPrimes {
       primes[0] = 2;
       int position, num = 3;
       do {
+         position = 0;
          do {
-            
+            if (num % primes[position] == 0) {
+               
+            }
+            position++;
+            if (primes[position == 0) {
+               
+            }
          }
          while(primes[position] != 0);
+         num += 2;
       }
       while (primes[primes.length - 1] == 0);
    }

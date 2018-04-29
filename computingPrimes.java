6f8eb8dddd611b23a8c0e53c719d5a72cc2665c0
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 14:47:46 -0600

    Added skeleton code for loop

--- computingPrimes.java
+++ computingPrimes.java
@@ -5,5 +5,12 @@ public class computingPrimes {
       int[] primes = new int[1000];
       primes[0] = 2;
       int position, num = 3;
+      do {
+         do {
+            
+         }
+         while(primes[position] != 0);
+      }
+      while (primes[primes.length - 1] == 0);
    }
 }

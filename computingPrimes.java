42ed820ca2df213e2baf1127014e30c0d999fe81
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 14:45:28 -0600

    Created necessary variables

--- computingPrimes.java
+++ computingPrimes.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 public class computingPrimes {
    public static void main(String args[]) {
-      
+      int[] primes = new int[1000];
+      primes[0] = 2;
+      int position, num = 3;
    }
 }
